7774514d09f6c6d4b99df1ca8826e112a6de6200
oxnildo <osnildosjunior@gmail.com>
2026-09-02 21:35:36 -0300

    js atualizado 1

---
+++ projeto1/js/app.js
@@ -0,0 +1,18 @@
+console.log("JavaScript carregando!")
+
+const busca = document.querySelector("#busca");
+
+const cards = document.querySelectorAll(".projeto-card");
+
+const botoes = document.querySelectorAll(".btn-filtro");
+
+console.log(busca);
+console.log(cards);
+console.log(botoes);
+
+botoes.forEach(function(botao){
+botao.addEventListener('click', function() {
+    console.log("cliquei!");
+    alert("cliquei!")
+    });
+});
